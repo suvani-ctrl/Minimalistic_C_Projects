c745806553875cb874d2dd671e04ca0f3f1e1f9a
suvani-ctrl <sssubbbi1234@gmail.com>
2024-12-07 23:05:56 +0545

    Update Hexdump.c
    updateeeeeeeeee

--- Hexdump.c
+++ Hexdump.c
@@ -3,47 +3,83 @@
 #include <stdint.h>
 #include <unistd.h>
 
-#define MAX_BUFF_SIZE 1024 * 1 //500KB
+
+#define MAX_BUFF_SIZE 1024*500
 
 typedef size_t usize;
 typedef uint8_t u8;
 typedef uint32_t u32;
 
-
-//function to check the file's existence
+void hexdump(char *buffer, usize file_size)
+{
+  
+}
 
 usize file_open_and_read(char *filepath, char *buffer, usize size)
 {
-    
 
-//function to check the file's existence
-if (access(filepath, F_OK) != 0)
-{
-    fprintf(stderr, "[+] THE FILE DOES NOT EXIST!\n", filepath);
-    exit(1);
+    printf("%s\n", filepath);
+    if (access(filepath,F_OK) == 0)
+    {
+        printf("File exists");
+    }
+    else
+    {
+        fprintf(stderr,"(+)File doesnt exists!!");
+        exit(1);
+    }
+
+    FILE *f = fopen(filepath, "r");
+    if(f == NULL)
+    {
+        fprintf(stderr,"(+) COULD NOT OPEN FILE!");
+        exit(1);
+    }
+    else{
+        //becus we cant access file more than allcated!
+
+        usize file_size;
+        fseek(f,0L, SEEK_END);
+        file_size = ftell(f);
+        fseek(f,0L,SEEK_SET);
+        printf("%zu\n", file_size);
+        if(file_size <= 0)
+        {
+            fprintf(stderr,"[+] Could not open file '%s'", filepath);
+            exit(1);
+        }
+        else if(file_size > size)
+
+        {
+            fprintf(stderr,"(+) file size is bigger than allowed!")
+            exit(1);
+        }
+
+        //READING THE FILES IN MEMORY 
+        fread(buffer, sizeof(char), file_size,f)
+        {
+            fclose(f);
+        }
+        return file_size;
+
+    }
+    return 0;
 }
 
-}
 
-void hexdump(char *buffer, usize file_size)
-{
-
-}
 
 int main(int argc, char **argv)
 {
-    printf("%d\n",argc);
-    if(argc != 2)
-    {
-    fprintf(stderr, "Usage is : %s\n", argv[0]);
+   if(argc != 2)
+   {
+    fprintf(stderr, "Usage: %s <file>\n", argv[0]);
     exit(1);
-    }
-    char *filepath = argv[1];
-    char buffer[MAX_BUFF_SIZE] = {0};
-    
-    usize file_size = file_open_and_read(filepath, buffer, MAX_BUFF_SIZE);
-    hexdump(buffer,file_size);
+   }
 
+   char *filepath = argv[1];
+   char buffer[MAX_BUFF_SIZE] = {0};
+
+   usize file_size = file_open_and_read(filepath, buffer, MAX_BUFF_SIZE);
 
     return 0;
 }
