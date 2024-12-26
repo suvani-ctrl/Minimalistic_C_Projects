5aae2dfd8cbe5eb2af5aa16555520ec2a538a02f
suvani-ctrl <sssubbbi1234@gmail.com>
2024-12-26 13:11:58 +0545

    Update basee64.c
    updated code that prints in binary (ASCII TO BINARY) base64 code half completion

--- basee64.c
+++ basee64.c
@@ -13,62 +13,68 @@ Instructions for Base64 Encoding in C
     Write the Base64-encoded data to the output file.
     Close both the input and output files.
 */
-
 #include <stdio.h>
 #include <stdlib.h>
+
 int file_existence(const char *filename)
 {
-    FILE *file = fopen(filename,"rb");
+    FILE *file = fopen(filename, "rb");
     if (file)
     {
         fclose(file);
         return 1;
     }
     else
-    {   
-        perror("File doesnot exist");
+    {
+        perror("File does not exist");
         return 0;
     }
 }
 
-
-size_t reader_function(FILE *file, char *buffer)  
-{
-    return fread(buffer, 1, 3,file);
-   
-}
-
-
 int main()
-{   
+{
     FILE *file;
     char buffer[3];
     size_t bytes_read;
     char filename[100];
 
-
-    printf("Enter the filename:");
+    printf("Enter the filename: ");
     scanf("%s", filename);
 
-
-    if (file_existence(filename)) {
-    file = fopen(filename, "rb");
-    
-    if (file) {
-        bytes_read = reader_function(file, buffer);
+    if (file_existence(filename))
+    {
+        file = fopen(filename, "rb");
         
-        for (size_t i = 0; i < bytes_read; i++) {
-            printf("%02x", buffer[i]);
+        if (file)
+        {
+            while ((bytes_read = fread(buffer, 1, 3, file)) > 0)
+            {
+     
+                for (size_t i = 0; i < bytes_read; i++)
+                {
+                    unsigned char byte = buffer[i];
+                    for (int j = 7; j >= 0; j--)
+                    {
+                        printf("%d", (byte >> j) & 1); //each bit (0 or 1)
+                    }
+                    printf(" "); // Space between each byte's binary representation
+                }
+                printf("\n"); // New line after each chunk of 3 bytes
+            }
+
+            fclose(file);
         }
-        printf("\n");
-        
-        fclose(file);
-    } else {
-        printf("Error opening the file!\n");
+        else
+        {
+            printf("Error opening the file!\n");
+        }
     }
-} else {
-    printf("File does not exist or can't be opened!\n");
+    else
+    {
+        printf("File does not exist or can't be opened!\n");
+    }
+
+    return 0;
 }
 
-return 0;
-}
+
